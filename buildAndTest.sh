150d0263c4aa515450d26c9ebc306238788a23b9
user <S.Mehran M.Ziabary>
2018-06-14 12:36:33 +0430

    build script updated

--- buildAndTest.sh
+++ buildAndTest.sh
@@ -22,8 +22,26 @@
 
 pushd $(dirname $0)
 
-if [ $1 == "full" ] && [ -z "$3" ]; then
-    echo "third parameter as PREFIX is obligatory when building in full mode"
+BuildType=$1
+BuildCriteria=$2
+BaseOutput=$3
+
+
+function showHappy(){
+    echo -e "\n\e[32m[$(date +"%Y/%m/%d %H:%M:%S")]: $@\e[39m\n"
+}
+function showError(){
+    echo -e "\n\e[31m[$(date +"%Y/%m/%d %H:%M:%S")]: $@\e[39m\n"
+}
+function showWarn(){
+    echo -e "\n\e[33m[$(date +"%Y/%m/%d %H:%M:%S")]: $@\e[39m\n"
+}
+function showInfo(){
+    echo -e "\n\e[36m[$(date +"%Y/%m/%d %H:%M:%S")]: $@\e[39m\n"
+}
+
+if [ "$BuildType" == "full" ] && [ -z "$BaseOutput" ]; then
+    showError "third parameter as PREFIX is obligatory when building in full mode"
     exit
 fi
 
@@ -37,89 +55,112 @@ QMAKE_COMMAND=qmake-qt5
 if [ -z "$(which $QMAKE_COMMAND 2> /dev/null)" ]; then
   QMAKE_COMMAND=qmake
   if [ -z "$(which $QMAKE_COMMAND 2> /dev/null)" ]; then
-    echo -e "\n\e[31m!!!!!!!!!!!!!!!!! QMake version 5 is needed to compile the project! !!!!!!!!!!!!!!!! \e[39m\n"
+    showError "!!!!!!!!!!!!!!!!! QMake version 5 is needed to compile the project! !!!!!!!!!!!!!!!!"
     exit 1
   fi
 fi
 
-echo "Using $QMAKE_COMMAND and $N_CORES_TO_USE processing cores for build ..."
+showInfo "Using $QMAKE_COMMAND and $N_CORES_TO_USE processing cores for build ..."
 
-echo "Building SubModules"
-mkdir -p out
-if [ -z "$3" ]; then
-    BaseOutput=$(readlink -f ./out)
-else
+if [ -n "$3" ]; then
     BaseOutput=$3
+else
+    path=$(realpath -s `pwd`)
+    while [[ $path != / ]]; do
+        if [ -f "$path/Project.pri" ]; then
+            BaseOutput=$path/out
+        else
+            break
+        fi
+        path="$(realpath -s "$path"/..)"
+    done
 fi
 
-echo "Base Output Path will be $BaseOutput"
-
+mkdir -p $BaseOutput
+showInfo "Base Output Path will be $BaseOutput"
 
 function installTargomanProject(){
     pushd $1 && \
-    if [ -z "$2" ];then FirstArg="NULL"; else FirstArg="$2"; fi && \
-    if [ -z "$3" ];then SecondArg="NULL"; else SecondArg="$3"; fi && \
-    ./buildAndTest.sh "$FirstArg" "$SecondArg" "$BaseOutput" && \
+    if [ -z "$BuildType" ];then FirstArg="NULL"; else FirstArg="$BuildType"; fi && \
+    if [ -z "$BuildCriteria" ];then SecondArg="NULL"; else SecondArg="$BuildCriteria"; fi && \
+    ./buildAndTest.sh $FirstArg $SecondArg "$BaseOutput" && \
     make install && \
     popd
 }
 
-echo "Building Module"
-#TODO -Werror
-
-BasePath=$BaseOutput
-export LD_LIBRARY_PATH="$BasePath/out/lib"
-if [ "$2" != "release" ] ; then
-  QMAKE_CONFIG="CONFIG+=debug CONFIG+=WITH_QJsonRPC PREFIX=${@:3}"
-else
-  QMAKE_CONFIG="CONFIG+=WITH_QJsonRPC PREFIX=${@:3}"
+if [ -f ".gitmodules" ]; then
+    showInfo "Building SubModules"
+    SubModules=$(cat .gitmodules | grep "\[submodule" | cut -d "\"" -f 2)
+    for Module in $SubModules; do
+        installTargomanProject $Module
+    done
 fi
 
-if [ "$1" == "full" ]; then
-    echo "Building in full Mode with: $QMAKE_COMMAND $QMAKE_CONFIG"
+
+if [ $? -ne 0 ]; then
+   exit
+fi
+
+showInfo "Building Module"
+#TODO -Werror
+
+if [ "$BuildCriteria" != "release" ] ; then
+  QMAKE_CONFIG="CONFIG+=debug CONFIG+=WITH_QJsonRPC PREFIX=$BaseOutput"
+elif [ "$BuildCriteria" != "distclean" ] ; then
+  QMAKE_CONFIG="CONFIG+=WITH_QJsonRPC PREFIX=$BaseOutput"
+else
+  QMAKE_CONFIG="CONFIG+=WITH_QJsonRPC PREFIX=$BaseOutput"
+fi
+
+if [ "$BuildType" == "full" ]; then
+    showInfo "Building in full Mode with: $QMAKE_COMMAND $QMAKE_CONFIG"
     rm -rf out
     rm -f `find ./ -name 'Makefile*'`
-    mkdir -p out/include
+    mkdir -p out
     if [ -f *.pro ]; then
         $QMAKE_COMMAND $QMAKE_CONFIG
     fi
     make -j $N_CORES_TO_USE
     if [ $? -ne 0 ];then
-        echo -e "\n\e[31m!!!!!!!!!!!!!!!!! Build Has failed!!!!!!!!!!!!!!!! \e[39m\n"
+        showError "!!!!!!!!!!!!!!!!! Build Has failed!!!!!!!!!!!!!!!!"
         exit 1;
     else
-        echo -e "\n\e[32m Module Compiled Successfully\e[39m\n"
-        if [ "$2" == "test" ];then
-            TestProgram=$(echo $BasePath/out/unitTest/unitTest_)
+        showHappy "Module Compiled Successfully"
+        if [ "$BuildCriteria" == "test" ];then
+            export LD_LIBRARY_PATH="$BaseOutput/lib"
+            TestProgram=$(echo $BaseOutput/unitTest/unitTest_)
             if [ -f  "$TestProgram" ]; then
-            eval "$TestProgram"
-            if [ $? -ne 0 ]; then
-                echo -e "\n\e[31m!!!!!!!!!!!!!!!!Testing Failed!!!!!!!!!!!!!!!! \e[39m\n"
-                exit 0
-            else
-                echo -e "\e[0;34m Testing Finished Successfully :) \e[39m\n"
-            fi
+                eval "$TestProgram"
+                if [ $? -ne 0 ]; then
+                    showError "!!!!!!!!!!!!!!!!Testing Failed!!!!!!!!!!!!!!!!"
+                    exit 0
+                else
+                    showHappy "Testing Finished Successfully :)"
+                fi
             fi
         fi
     fi
+elif [ "$BuildType" == "distclean" ]; then
+    showInfo "cleaning project with: $QMAKE_COMMAND $QMAKE_CONFIG"
+    rm -rf "$BaseOutput"
+    make distclean
+    rm -rvf `find ./ -name MakeFile`
+    rm -rvf `find ./ -name out`
+    showHappy "Module Distcleaned Successfully"
 else
-    echo "Building incremental with: $QMAKE_COMMAND $QMAKE_CONFIG"
-    mkdir -p out/include
+    showInfo "Building incremental with: $QMAKE_COMMAND $QMAKE_CONFIG"
+    mkdir -p out
     $QMAKE_COMMAND $QMAKE_CONFIG
     make -j $N_CORES_TO_USE
     if [ $? -ne 0 ];then
-    echo -e "\n\e[31m!!!!!!!!!!!!!!!!! Build Has failed!!!!!!!!!!!!!!!! \e[39m\n"
-    exit 1;
+        showError "!!!!!!!!!!!!!!!!! Build Has failed!!!!!!!!!!!!!!!!"
+        exit 1;
     else
-    echo -e "\n\e[32m Module Compiled Successfully\e[39m\n"
+        showHappy "Module Compiled Successfully"
     fi
 fi
 
-#if [ -d "./scripts" ]; then
-#    mkdir -p $BaseOutput/scripts
-#    for Path in $(ls ./scripts/); do
-#    ln -snf $readlink("./scripts/$Path") "./out/scripts/"
-#    done
-#fi
+
 
 popd
+
